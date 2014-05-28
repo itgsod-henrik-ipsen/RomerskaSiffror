a6349bf48d6c830663d903d41e0b6d2e25e9bec4
itgsod-henrik-ipsen <henrik.ipsen@elev.it-gymnasiet.se>
2014-05-28 15:35:44 +0200

    added output

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -3,4 +3,6 @@ def romanize(number)
     raise ArgumentError, 'can not encode negative number' if number <= 0
 
     numbers = {1000 => 'M', 500 => 'D', 100 => 'C', 50 => 'L', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I'}
+    output = String.new
+    
 end
