db42021ec5f90693cfe284d0a4205fa504923683
itgsod-henrik-ipsen <henrik.ipsen@elev.it-gymnasiet.se>
2014-05-28 15:34:00 +0200

    added number hash

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -1,4 +1,6 @@
 def romanize(number)
     raise ArgumentError, 'can not encode zero' if number.zero?
-    raise ArgumentError, 'can not encode negative number' if number <= 0 
+    raise ArgumentError, 'can not encode negative number' if number <= 0
+
+    numbers = {1000 => 'M', 500 => 'D', 100 => 'C', 50 => 'L', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I'}
 end
