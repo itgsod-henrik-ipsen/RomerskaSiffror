234c42a4f560dc8c3648c21022d020ae534f9a5f
itgsod-henrik-ipsen <henrik.ipsen@elev.it-gymnasiet.se>
2014-05-28 15:59:42 +0200

    made it code single digit numbers

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -2,8 +2,20 @@ def romanize(number)
     raise ArgumentError, 'can not encode zero' if number.zero?
     raise ArgumentError, 'can not encode negative number' if number <= 0
 
-    numbers = {1000 => 'M', 500 => 'D', 100 => 'C', 50 => 'L', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I'}
+    numbers = {1000 => 'M', 500 => 'D', 100 => 'C', 90 => 'XC' 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I'}
     output = String.new
+
+    while number != 0
+        numbers.each do |key, value|
+            if number >= key
+                output += value
+                number -= key
+                break
+            end
+        end
+    end
 
     output
 end
+
+
