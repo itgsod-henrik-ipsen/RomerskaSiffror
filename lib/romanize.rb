76548b8d5a1df00e6e2b4c82eae3d6ae39bb72f0
itgsod-henrik-ipsen <henrik.ipsen@elev.it-gymnasiet.se>
2014-05-28 16:13:06 +0200

    made it code four digits numbers

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -2,7 +2,7 @@ def romanize(number)
     raise ArgumentError, 'can not encode zero' if number.zero?
     raise ArgumentError, 'can not encode negative number' if number <= 0
 
-    numbers = {1000 => 'M', 900 => 'CM', 500 => 'D', 100 => 'C', 90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I'}
+    numbers = {1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C', 90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I'}
     output = String.new
 
     while number != 0
